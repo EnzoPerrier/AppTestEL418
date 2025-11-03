4768ae0a5887288473eca71ebebe01f1a59b9391
Enzo <katiagauberti@gmail.com>
2025-11-03 13:55:40 +0100

    MAJ logo dans le .exe

--- AppTestEL418/AppTestEL418/obj/Debug/net8.0-windows/AppTestEL418.AssemblyInfo.cs
+++ AppTestEL418/AppTestEL418/obj/Debug/net8.0-windows/AppTestEL418.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3663308f361f989a83bbdaaf8d35e710fc841105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4682bd1ccc704d9c236162470462c9661341fbe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
