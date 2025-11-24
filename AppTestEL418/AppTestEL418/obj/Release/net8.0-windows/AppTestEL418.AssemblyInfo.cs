e1ec0b8aab155b01fccbe5ed335552e058aae869
Enzo <katiagauberti@gmail.com>
2025-11-24 08:03:43 +0100

    Modifs mineures

--- AppTestEL418/AppTestEL418/obj/Release/net8.0-windows/AppTestEL418.AssemblyInfo.cs
+++ AppTestEL418/AppTestEL418/obj/Release/net8.0-windows/AppTestEL418.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5d288482f3261d5dc7fb84d5b9c624e01b4467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4dd6482dfd6fe212130ed1551ded66ee78933d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
