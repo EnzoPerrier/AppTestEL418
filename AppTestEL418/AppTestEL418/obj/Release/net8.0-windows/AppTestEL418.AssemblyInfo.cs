0d57c2c2b0c17801fbec5cd375861806d369c2e7
Enzo <katiagauberti@gmail.com>
2026-02-27 14:42:57 +0100

    Fix bug: Chiffres du compte à rebours pas centrés

--- AppTestEL418/AppTestEL418/obj/Release/net8.0-windows/AppTestEL418.AssemblyInfo.cs
+++ AppTestEL418/AppTestEL418/obj/Release/net8.0-windows/AppTestEL418.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfd97c0d69b3511f65a241d12e733a1f415748e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86bb2693c8bb2aca896b251af6288ee94da92be")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTestEL418")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
